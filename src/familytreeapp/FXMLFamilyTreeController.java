18076b996a7f3e12dc6478c69aaede5ffd5ac58d
Jose Corral <angel.corral71@gmail.com>
2015-04-24 20:47:33 -0500

    Needs more work

--- src/familytreeapp/FXMLFamilyTreeController.java
+++ src/familytreeapp/FXMLFamilyTreeController.java
@@ -6,46 +6,81 @@
 package familytreeapp;
 
 import daBandit.com.FamilyMember;
+import java.awt.Image;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.image.ImageView;
 
 /**
  *
  * @author angel
  */
 public class FXMLFamilyTreeController implements Initializable {
-    private FamilyMember angel;
+    public FamilyMember angel;
+    public FamilyMember maria;
+    public FamilyMember jessie;
+    public FamilyMember yola;
+    public FamilyMember edgar;
+    public FamilyMember eric;
+    public FamilyMember cisco;
+    public ObservableList mariaChildren;
     private TreeItem<FamilyMember> root;
+    public  Node rootIcon;
     
     
     @FXML
     private TreeView<FamilyMember> familyTreeView;
     
-    private void handleButtonAction(ActionEvent event) {
-        
-    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        
+       // Image test = ;
+        rootIcon = new ImageView("resources/Leaf.png");
+        buildList();
         buildTree();
         
         
         
         
     }   
+    public void buildList(){
+        maria = new FamilyMember("Maria", "Francisco", "61");
+        angel = new FamilyMember("Angel", "none", "43");
+        jessie = new FamilyMember("Jessie", "none", "42");
+        yola = new FamilyMember("Yola", "Jose", "41");
+        edgar = new FamilyMember("Edgar", "Lucy", "35");
+        eric = new FamilyMember("Eric", "none", "30");
+        cisco = new FamilyMember("Cisco", "none", "28");
+        
+        mariaChildren = FXCollections.observableArrayList();
+        mariaChildren.addAll(angel, jessie, yola, edgar, eric, cisco);
+        maria.setChildren(mariaChildren);
+        System.out.println(maria.toString());
+        
+       
+       
+        
+        
+    }
     
     public void buildTree(){
-        FamilyMember angel = new FamilyMember("Angel Corral","none", "43");
-        TreeItem<FamilyMember> angelTree = new TreeItem<>();
-        root = new TreeItem<FamilyMember>(angel);
-        
+        //FamilyMember angel = new FamilyMember("Angel Corral","none", "43");
+         
+         
+        root = new TreeItem<>(maria, rootIcon);
+        System.out.println(maria.toString());
+        TreeItem<FamilyMember> angelTree = new TreeItem<>(angel);
+        TreeItem<FamilyMember> jessieTree = new TreeItem<>(jessie);
         familyTreeView.setRoot(root);
+        root.getChildren().addAll(angelTree, jessieTree);
         
     }
     
